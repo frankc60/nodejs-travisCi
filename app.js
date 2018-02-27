26481d5fec94f907d568eef9f447f743c2d80711
frankc60 <frankc60@gmail.com>
2018-02-27 20:51:01 +0000

    text instructions

--- app.js
+++ app.js
@@ -4,3 +4,8 @@ http.createServer(function (req, res) {
   res.end('Hello Travis!\n');
 }).listen(1337, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:1337/');
+
+//when test run (npm test), it will use jshint to find issues,, if you take off a semi-colon, it will report this failure.
+
+//if you push to github, travis will run the tests remotely and email me the failure, once fixed, it will report it has been fixed,
+//and the github icon build, will go green and say passing :)
